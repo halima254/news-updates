5e3da62461085a7c5b00be1592d06289523ef717
halima254 <halima.yahya@student.moringaschool.com>
2021-09-12 16:51:30 +0300

    remove source_test file from models to root folder

---
+++ source_test.py
@@ -0,0 +1,18 @@
+import unittest
+from app.models import source
+
+Source = source.Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the source class
+    '''
+    def setUp(self):
+        self.new_source = Source('test name','test author','test title','test description')
+        
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Source))
+        
+if __name__ =='__main__':
+    unittest.main()
